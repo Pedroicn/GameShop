8d25e9250d9ff1e978b9d226a8b836ed0903f639
Pedro Nunes <pedro.icn88@gmail.com>
2023-05-25 09:06:08 -0300

    feat: some initial style settings

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from "react";
+import style from "./Header.module.scss";
+import { Link } from "react-router-dom";
+
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className={ style.header }>
+        <div className={ style.logo }>
+          <Link to="/">
+            <h2>
+              GEEK<span>Shop</span>
+            </h2>
+          </Link>
+        </div>
+      </div>
+    </header>
   )
 }
 
