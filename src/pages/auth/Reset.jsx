3c01fccec16dbb07f8a94a4590a8adafb7e6ceac
Pedro Nunes <pedro.icn88@gmail.com>
2023-06-13 15:49:08 -0300

    feat: build the login, register and reset pages with their respective styles

--- src/pages/auth/Reset.jsx
+++ src/pages/auth/Reset.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import resetImg from "../../assets/reset.png";
+import Card from "../../components/card/Card";
+import styles from "./auth.module.scss";
 
 function Reset() {
-  return <div>Reset</div>;
+  return (
+    <section className={`container ${styles.auth}`}>
+      <div className={styles.img}>
+        <img src={resetImg} alt="reset" width="400" />
+      </div>
+      <Card>
+        <div className={styles.form}>
+          <h2>Reset Password</h2>
+
+          <form>
+            <input type="text" placeholder="Email" required />
+
+            <button className="--btn --btn-primary --btn-block">
+              Reset Password
+            </button>
+            <div className={styles.links}>
+              <p>
+                <Link to="/login">Login</Link>
+              </p>
+              <p>
+                <Link to="/register">Register</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </Card>
+    </section>
+  );
 }
 
 export default Reset;
