21d539e12120d9e5e222b13426027e9356ed771f
Pedro Nunes <pedro.icn88@gmail.com>
2023-06-13 18:34:18 -0300

    feat: built user registration logic with firebase

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom";
-import loaderImg from "../../assets/loader.gif";
+import loaderImg from "../../assets/loader.png";
 import styles from "./Loader.module.scss";
 
 function Loader() {
   return ReactDOM.createPortal(
     <div className={styles.wrapper}>
       <div className={styles.loader}>
-        <img src={loaderImg} alt="Loading..." />
+        <img src={loaderImg} alt="Loading..." width={100} />
       </div>
     </div>,
     document.getElementById("loader")
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,20 +1,73 @@
 import styles from "./auth.module.scss";
 import registerImg from "../../assets/register.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Card from "../../components/card/Card";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import Loader from "../../components/loader/Loader";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    if (password !== confPassword) {
+      toast.error("Passwords do not match.");
+    }
+
+    setIsLoading(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setIsLoading(false);
+        toast.success("Registration Successful");
+        navigate("/login");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        setIsLoading(false);
+      });
+  };
+
   return (
     <section className={`container ${styles.auth}`}>
+      {isLoading && <Loader />}
+      <ToastContainer />
       <Card>
         <div className={styles.form}>
           <h2>Register</h2>
 
-          <form>
-            <input type="text" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            <input type="password" placeholder="Confirm Password" required />
-            <button className="--btn --btn-primary --btn-block">
+          <form onSubmit={registerUser}>
+            <input
+              type="text"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              required
+              value={confPassword}
+              onChange={(e) => setConfPassword(e.target.value)}
+            />
+            <button type="submit" className="--btn --btn-primary --btn-block">
               Register
             </button>
           </form>
