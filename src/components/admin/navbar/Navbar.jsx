2397f07ccc7b98eef121dae94baf2d4f511343d4
Pedroicn <pedro.icn88@gmail.com>
2023-06-26 16:34:23 -0300

    feat: add navbar links

--- src/components/admin/navbar/Navbar.jsx
+++ src/components/admin/navbar/Navbar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import styles from "./Navbar.module.scss";
+import { useSelector } from "react-redux";
+import { selectUserName } from "../../../redux/features/authFeature";
+import { FaUserCircle } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+
+const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const userName = useSelector(selectUserName);
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.user}>
+        <FaUserCircle size={40} color="#fff" />
+        <h4>{userName}</h4>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/admin/home" className={activeLink}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/all-products" className={activeLink}>
+              All Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/add-products" className={activeLink}>
+              Add Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/orders" className={activeLink}>
+              Orders
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
