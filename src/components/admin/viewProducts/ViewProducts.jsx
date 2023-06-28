9b65d1e5c1d03f9a0fd00cc5b322981f5f01c01f
Pedroicn <pedro.icn88@gmail.com>
2023-06-28 11:16:12 -0300

    feat: add funcionality to get products from firebase and display them on the page

--- src/components/admin/viewProducts/ViewProducts.jsx
+++ src/components/admin/viewProducts/ViewProducts.jsx
@@ -1,7 +1,83 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import styles from "./ViewProducts.module.scss";
+import { toast } from "react-toastify";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../../../firebase/config";
+import { Link } from "react-router-dom";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 function ViewProducts() {
-  return <div>ViewProducts</div>;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = () => {
+    setIsLoading(true);
+    try {
+      const productsRef = collection(db, "products");
+      const q = query(productsRef, orderBy("createdAt", "desc"));
+
+      onSnapshot(q, (snapshot) => {
+        // console.log(snapshot.docs);
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+  return (
+    <div className={styles.table}>
+      <h2>All Products</h2>
+      {products.length === 0 ? (
+        <p>No products found!</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Serial Number</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          {products.map((product, index) => {
+            const { id, name, price, imageURL, category } = product;
+            return (
+              <tbody>
+                <tr key={id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <img src={imageURL} alt={name} style={{ width: "100px" }} />
+                  </td>
+                  <td>{name}</td>
+                  <td>{category}</td>
+                  <td>{`R$ ${price}`}</td>
+                  <td>
+                    <Link to="/admin/add-products">
+                      <FaEdit size={20} color="green" />
+                    </Link>
+                    &nbsp;
+                    <FaTrashAlt size={18} color="red" />
+                  </td>
+                </tr>
+              </tbody>
+            );
+          })}
+        </table>
+      )}
+    </div>
+  );
 }
 
 export default ViewProducts;
