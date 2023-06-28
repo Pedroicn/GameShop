a4df7afb09b7bab246fa980d6307d2765e1df478
Pedroicn <pedro.icn88@gmail.com>
2023-06-28 14:12:19 -0300

    feat: add funcionality to delete product

--- src/components/admin/viewProducts/ViewProducts.jsx
+++ src/components/admin/viewProducts/ViewProducts.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from "react";
 import styles from "./ViewProducts.module.scss";
 import { toast } from "react-toastify";
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import { db } from "../../../firebase/config";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { db, storage } from "../../../firebase/config";
 import { Link } from "react-router-dom";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import Loader from "../../loader/Loader";
+import { deleteObject, ref } from "firebase/storage";
+import Notiflix from "notiflix";
+
 function ViewProducts() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -34,49 +45,96 @@ function ViewProducts() {
       toast.error(error.message);
     }
   };
+
+  const confirmDelete = (id, imageURL) => {
+    Notiflix.Confirm.show(
+      "Delete Product!!!",
+      "You are about to delete this product.",
+      "Delete",
+      "Cancel",
+      function okCb() {
+        deleteObject(id, imageURL);
+      },
+      function cancelCb() {
+        console.log("Delete canceled");
+      },
+      {
+        width: "320px",
+        borderRadius: "8px",
+        // titleColor: "black"
+        // okButtonBackground: "orangered"
+        // cssAnimationStyle: "fade"
+        // etc...
+      }
+    );
+  };
+
+  const deleteProduct = async (id, imageURL) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+      const storageRef = ref(storage, imageURL);
+
+      await deleteObject(storageRef);
+      toast.success("Product delete successfully");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
-    <div className={styles.table}>
-      <h2>All Products</h2>
-      {products.length === 0 ? (
-        <p>No products found!</p>
-      ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Serial Number</th>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Category</th>
-              <th>Price</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          {products.map((product, index) => {
-            const { id, name, price, imageURL, category } = product;
-            return (
-              <tbody>
-                <tr key={id}>
-                  <td>{index + 1}</td>
-                  <td>
-                    <img src={imageURL} alt={name} style={{ width: "100px" }} />
-                  </td>
-                  <td>{name}</td>
-                  <td>{category}</td>
-                  <td>{`R$ ${price}`}</td>
-                  <td>
-                    <Link to="/admin/add-products">
-                      <FaEdit size={20} color="green" />
-                    </Link>
-                    &nbsp;
-                    <FaTrashAlt size={18} color="red" />
-                  </td>
-                </tr>
-              </tbody>
-            );
-          })}
-        </table>
-      )}
-    </div>
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.table}>
+        <h2>All Products</h2>
+        {products.length === 0 ? (
+          <p>No products found!</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Serial Number</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            {products.map((product, index) => {
+              const { id, name, price, imageURL, category } = product;
+              return (
+                <tbody>
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={imageURL}
+                        alt={name}
+                        style={{ width: "100px" }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>{`R$ ${price}`}</td>
+                    <td className={styles.icons}>
+                      <Link to="/admin/add-products">
+                        <FaEdit size={20} color="green" />
+                      </Link>
+                      &nbsp;
+                      <FaTrashAlt
+                        size={18}
+                        color="red"
+                        onClick={() => confirmDelete(id, imageURL)}
+                      />
+                    </td>
+                  </tr>
+                </tbody>
+              );
+            })}
+          </table>
+        )}
+      </div>
+    </>
   );
 }
 
