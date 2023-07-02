04d45fd99566d3d67f662779af2fa98069f10398
Pedroicn <pedro.icn88@gmail.com>
2023-07-02 09:17:58 -0300

    redirect user on checkout

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -12,13 +12,24 @@ import {
 import { auth } from "../../firebase/config";
 import { ToastContainer, toast } from "react-toastify";
 import Loader from "../../components/loader/Loader";
+import { useSelector } from "react-redux";
+import { selectPreviousURL } from "../../redux/features/cartFeature";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  const previousURL = useSelector(selectPreviousURL);
+
   const navigate = useNavigate();
+
+  const redirectUser = () => {
+    if (previousURL.includes("cart")) {
+      return navigate("/cart");
+    }
+    navigate("/");
+  };
 
   const loginUser = (e) => {
     e.preventDefault();
@@ -28,7 +39,7 @@ function Login() {
         // const user = userCredential.user;
         setIsLoading(false);
         toast.success("Login Successfull");
-        navigate("/");
+        redirectUser();
       })
       .catch((error) => {
         setIsLoading(false);
@@ -43,7 +54,7 @@ function Login() {
       .then((result) => {
         // const user = result.user;
         toast.success("Login Successfully");
-        navigate("/");
+        redirectUser();
       })
       .catch((error) => {
         toast.error(error.message);
