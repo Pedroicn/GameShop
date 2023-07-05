7b0e9a8dcae601147ebc6f5f516dd09aaa17ca75
Pedroicn <pedro.icn88@gmail.com>
2023-07-05 10:54:50 -0300

    feat: create initial settings for server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+
+const cors = require("cors");
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+const app = express();
+app.use(cors());
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to gameshop!");
+});
+
+const PORT = process.env.PORT || 4242;
+app.listen(PORT, () => console.log(`Node server listening on port ${PORT}!`));
