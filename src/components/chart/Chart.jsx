76ecb3238cd6e4c9e563906c812098fb159885f6
Pedroicn <pedro.icn88@gmail.com>
2023-07-12 15:50:00 -0300

    feat: build order status chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,7 +1,86 @@
 import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import Card from "../card/Card";
+import { useSelector } from "react-redux";
+import { selectOrderHistory } from "../../redux/features/orderFeature";
+import styles from "./Chart.module.scss";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: false,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
 
 function Chart() {
-  return <div>Chart</div>;
+  const orders = useSelector(selectOrderHistory);
+
+  // Create a new array of order status
+  const array = [];
+  orders.map((item) => {
+    const { orderStatus } = item;
+    return array.push(orderStatus);
+  });
+
+  const getOrderCount = (arr, value) => {
+    return arr.filter((n) => n === value).length;
+  };
+
+  const [q1, q2, q3, q4] = [
+    "Order Placed...",
+    "Processing...",
+    "Shipped...",
+    "Delivered",
+  ];
+
+  const placed = getOrderCount(array, q1);
+  const processing = getOrderCount(array, q2);
+  const shipped = getOrderCount(array, q3);
+  const delivered = getOrderCount(array, q4);
+
+  const data = {
+    labels: ["Placed Orders", "Processing", "Shipped", "Delivered"],
+    datasets: [
+      {
+        label: "Order Count",
+        data: [placed, processing, shipped, delivered],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  return (
+    <div className={styles.charts}>
+      <Card cardClass={styles.card}>
+        <h3>Order Status Chart</h3>
+        <Doughnut data={data} />
+      </Card>
+    </div>
+  );
 }
 
 export default Chart;
